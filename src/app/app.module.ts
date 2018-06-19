a08952f1741743fbf76877503468b86d6277eaed
Even Hus <even@jodacare.com>
2018-06-19 13:03:02 +0200

    Added images and startpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
-
+import {appRoutes} from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
